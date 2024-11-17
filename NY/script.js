8bfacb733764cdcb97d90ad7174b4ae36e8fa2d8
flamesheep <90888812+flamesheep@users.noreply.github.com>
2024-11-18 04:24:12 +0800

    Update script.js

--- NY/script.js
+++ NY/script.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 判斷螢幕寬度，假設手機為螢幕寬度小於 768px
-    if (window.innerWidth < 768) {
-      // 如果當前頁面不是手機版網址，則導向手機版
-      if (window.location.href !== "http://flamesheep.github.io/OCweb/NY/m/") {
-        window.location.href = "http://flamesheep.github.io/OCweb/NY/m/";
-      }
-    } else {
-      // 如果當前頁面是手機版網址，則導向電腦版
-      if (window.location.href === "http://flamesheep.github.io/OCweb/NY/m/") {
-        window.location.href = "http://flamesheep.github.io/OCweb/NY";
-      }
-    }
-  });
+  // 判斷螢幕寬度，假設手機為螢幕寬度小於 768px
+  if (window.innerWidth < 768) {
+    // 裝置為手機，導向B網址
+    window.location.href = "http://flamesheep.github.io/OCweb/NY/m/";
+  } else {
+    // 裝置為電腦，導向A網址
+    window.location.href = "http://flamesheep.github.io/OCweb/NY";
+  }
+});

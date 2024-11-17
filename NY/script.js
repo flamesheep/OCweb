f966adb36683d49f89ab40442b92c8a9c797522d
flamesheep <whyicannot28@gmail.com>
2024-11-18 04:36:34 +0800

    upload js

--- NY/script.js
+++ NY/script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const currentUrl = window.location.href;
+  
   // 判斷螢幕寬度，假設手機為螢幕寬度小於 768px
-  if (window.innerWidth < 768) {
-    // 裝置為手機，導向B網址
+  if (window.innerWidth < 768 && !currentUrl.includes("/m/")) {
+    // 若裝置為手機且網址中不包含 "/m/"，則跳轉到手機版
     window.location.href = "http://flamesheep.github.io/OCweb/NY/m/";
-  } else {
-    // 裝置為電腦，導向A網址
+  } else if (window.innerWidth >= 768 && currentUrl.includes("/m/")) {
+    // 若裝置為電腦且網址中包含 "/m/"，則跳轉到桌面版
     window.location.href = "http://flamesheep.github.io/OCweb/NY";
   }
 });
